75e1b13a693061171f29549f4cdbfaadaa0b6c30
Panagiotis Kontos <65974766+Panoskontos@users.noreply.github.com>
2022-01-04 21:39:10 +0200

    Add files via upload

---
+++ autogui_scripts.py
@@ -0,0 +1,43 @@
+import pyautogui
+import time
+
+
+# it's good idea to start with 
+time.sleep(3)
+
+pyautogui.press('enter')
+
+
+# print(pyautogui.size())
+print(pyautogui.position())
+
+
+# move the mouse OVER TIME 3 seconds
+# pyautogui.moveTo(200,74,3)
+
+
+# Click
+# pyautogui.click(200, 74)
+# pyautogui.typewrite('https://www.youtube.com/watch?v=fNFzfwLM72c&ab_channel=BeeGeesVEVO')
+# pyautogui.typewrite(['enter'])
+# pyautogui.confirm('Boss everything is ready')
+
+# Right Click
+# pyautogui.rightClick(1330,235)
+
+# scroll down
+# pyautogui.click(1330,235)
+# pyautogui.scroll(-300)
+
+
+# mouse up and down
+# pyautogui.mouseDown(300,400, button='left')
+# pyautogui.moveTo(800,400)
+# pyautogui.mouseUp(300,400, button='left')
+
+# write
+# pyautogui.typewrite(["enter"])
+
+# press hot keys
+# pyautogui.hotkey("command", "down")
+
